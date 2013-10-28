ca40cfd7c9e1aacf1c4946285722c0be42dac5fd
Shuan Wang <shuanwang@gmail.com>
2013-10-28 00:23:57 -0700

    Tests now do not use plural that modifies natives
    Instead of using the function that extends String's native object,
    we will just use the provided plural function.
    
    Also changed the example to just use plural() rather than using String.

--- examples/stopius.js
+++ examples/stopius.js
@@ -1,6 +1,8 @@
+// create a new rule that converts any word ending
+// with 'p' and appends 'ius' to the word
+
 var plural = require('./index.js')
-  , monkeyPatch = plural.monkeyPatch()
 
 plural.addRule(/p{1,2}$/i, function(w) { return w + "ius" })
 
-console.log("stop".plural())
+console.log(plural("stop"))
